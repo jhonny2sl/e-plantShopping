13298c110bf433fa3aa6a831ed8d93f31ddc1f6a
jhonny2sl <jhonnytwosl@gmail.com>
2025-09-13 02:44:02 +0700

    Integrated Redux functionalities

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import { useSelector, useDispatch } from "react-redux";
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
+
+    const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    };
 
     const plantsArray = [
         {
